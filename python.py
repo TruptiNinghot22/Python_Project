d0840ee37d519dc539997fae7d77dbf03e9ca776
TruptiNinghot22 <140891147+TruptiNinghot22@users.noreply.github.com>
2023-11-06 16:15:34 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,103 @@
+def print_menu(day):
+    if day == 1:
+        return "Monday's Food Menu"
+    elif day == 2:
+        return "Tuesday Food Menu"
+    elif day == 3:
+        return "Wednesday Food Menu"
+    elif day == 4:
+        return "Thursday Food Menu"
+    elif day == 5:
+        return "Friday Food Menu"
+    elif day == 6:
+        return "Saturday Food Menu"
+    elif day == 7:
+        return "Sunday Food Menu"
+    else:
+        return "Invalid Day"
+
+def print_breakfast_menu(day):
+    if day in [1, 2, 3, 4, 5, 6, 7]:
+        print("Breakfast Menu")
+        price = 0
+        while True:
+            menu = input("Enter a breakfast menu:")
+            if menu == "poha":
+                price += 20
+            elif menu == "bread pakoda":
+                price += 30
+            elif menu == "itali":
+                price += 40
+            elif day == 5 and menu == "smosa":
+                price += 30
+            elif day == 6 and menu == "uttapa":
+                price += 70
+            elif day == 7 and menu == "dosa":
+                price += 60
+            else:
+                print("Invalid")
+            ans = input("Do you want to continue?(y/n)")
+            if ans != 'y':
+                break
+        print("Price Rs=" + str(price))
+    else:
+        print("Invalid Day")
+
+def print_lunch_menu(day):
+    if day in [1, 2, 3, 4, 5, 6, 7]:
+        if day == 1:
+            print("Lunch: Potato Vegetable, Flat Bread, Rice, Dal")
+        elif day == 2:
+            print("Lunch: Spinach Vegetable, Flat Bread, Rice, Dal")
+        elif day == 3:
+            print("Lunch: Cauliflower Vegetable, Flat Bread, Rice, Dal")
+        elif day == 4:
+            print("Lunch: Capsicum Vegetable, Flat Bread, Rice, Dal")
+        elif day == 5:
+            print("Lunch: Spinach Potato Vegetable, Flat Bread, Rice, Dal")
+        elif day == 6:
+            print("Lunch: Spinach Paneer Vegetable, Flat Bread, Rice, Dal")
+        elif day == 7:
+            print("Lunch: Kofte Vegetable, Flat Bread, Rice, Dal")
+    else:
+        print("Invalid Day")
+
+def print_dinner_menu(day):
+    if day in [1, 2, 3, 4, 5, 6, 7]:
+        if day in [1, 4]:
+            print("Dinner: Brinjal Vegetable, Flat Bread, Rice, Dal")
+        elif day in [2, 5]:
+            print("Dinner: Cabbage Vegetable, Flat Bread, Rice, Dal")
+        elif day in [3, 6]:
+            print("Dinner: Tomato Vegetable, Flat Bread, Rice, Dal")
+        elif day == 7:
+            print("Dinner: Gaur Vegetable, Flat Bread, Rice, Dal")
+    else:
+        print("Invalid Day")
+
+print("Week Days")
+print("1.Monday")
+print("2.Tuesday")
+print("3.Wednesday")
+print("4.Thursday")
+print("5.Friday")
+print("6.Saturday")
+print("7.Sunday")
+
+day = int(input("Enter a day:"))
+time = input("Enter a time(breakfast, lunch, dinner):")
+
+menu = print_menu(day)
+
+if menu != "Invalid Day":
+    print(menu)
+    if time == "breakfast":
+        print_breakfast_menu(day)
+    elif time == "lunch":
+        print_lunch_menu(day)
+    elif time == "dinner":
+        print_dinner_menu(day)
+else:
+    print("Invalid Day")
+
+print("Thank You")
